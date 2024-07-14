2385a6a2d946266731fe69543394579116fc533f
Lazygojo28 <joshuajoves10@gmail.com>
2024-07-14 21:46:02 +0800

    00_prefix.js

--- scripts/cmds/00_prefix.js
+++ scripts/cmds/00_prefix.js
@@ -15,13 +15,13 @@ module.exports = {
  if (event.body && event.body.toLowerCase() === "prefix") {
  return message.reply({
  body: `
-Yo, my prefix is [ 𓆩 ✓ 𓆪 ]\n
+Yo, my prefix is [ 𓆩 - 𓆪 ]\n
 𝗦𝗢𝗠𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗧𝗛𝗔𝗧 𝗠𝗔𝗬 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨:
-➥ ✓help [number of page] -> see commands
-➥ ✓sim [message] -> talk to bot
-➥ ✓callad [message] -> report any problem encountered
-➥ ✓help [command] -> information and usage of command\n\nHave fun using it enjoy!❤️\nBot Developer: https://www.facebook.com/swordigo.swordslush`,
- attachment: await global.utils.getStreamFromURL("https://i.imgur.com/M4luPbE.gif")
+➥ -help [number of page] -> see commands
+➥ -anya [message] -> talk to bot
+➥ -callad [message] -> report any problem encountered
+➥ -help [command] -> information and usage of command\n\nHave fun using it enjoy!❤️\nBot Developer: https://www.facebook.com/profile.php?id=100084525945365`,
+ attachment: await global.utils.getStreamFromURL("https://i.imgur.com/NIcn0PE.png")
  });
  }
  }
