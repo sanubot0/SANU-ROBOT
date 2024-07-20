0f7bc2dab3651bd2e2644975506aec1c5303d255
Lazygojo28 <joshuajoves10@gmail.com>
2024-07-20 16:52:50 +0800

    Update help.js

--- backups/backup_1.5.22/scripts/cmds/help.js
+++ backups/backup_1.5.22/scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "天 | 𝙔𝘼𝙕𝙆𝙔 𝘼𝙄";
+const doNotDelete = "天 | JULES BOT";
 /** 
 * @author NTKhang
 * @author: do not delete it
